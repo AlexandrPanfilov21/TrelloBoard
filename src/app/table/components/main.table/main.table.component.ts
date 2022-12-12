3e48b69b2ab5683b8b1bc945ac01e3bb3b662fc3
Александр Панфилов <panfilove4516@gmail.com>
2022-12-12 06:08:09 +0300

    feature (table): add main function and design application

--- src/app/table/components/main.table/main.table.component.ts
+++ src/app/table/components/main.table/main.table.component.ts
@@ -8,9 +8,19 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
 })
 export class MainTableComponent {
 
-  public todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep', 'Poebatsya'];
-
+  public todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
   public done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  public inProgress: string[] = [];
+  public review: string[] = [];
+
+  public itemTodo!: string;
+  public itemInProgress!: string;
+  public itemReview!: string;
+  public itemDone!: string;
+  public todoH2: string = 'To do';
+  public inProgressH2: string = 'In Progress';
+  public reviewH2: string = 'Review';
+  public doneH2: string = 'Done';
 
   drop (event: CdkDragDrop<string[]>): void {
     if (event.previousContainer === event.container) {
@@ -25,7 +35,55 @@ export class MainTableComponent {
     }
   }
 
-  addItems(): void {
+  addItemsInTodo(): void {
+    this.todo.push(this.itemTodo)
+    this.itemTodo = '';
+  }
 
+  addItemsInProgress(): void {
+    this.inProgress.push(this.itemInProgress)
+    this.itemInProgress = '';
+  }
+
+  addItemsInReview(): void {
+    this.review.push(this.itemReview)
+    this.itemReview = '';
+  }
+
+  addItemsInDone(): void {
+    this.done.push(this.itemDone)
+    this.itemDone = '';
+  }
+
+  deleteItemsTodo(value: string): void {
+    const index = this.todo.indexOf(value);
+
+    if (index >= 0) {
+      this.todo.splice(index, 1);
+    }
+  }
+
+  deleteItemsInProgress(value: string): void {
+    const index = this.inProgress.indexOf(value);
+
+    if (index >= 0) {
+      this.inProgress.splice(index, 1);
+    }
+  }
+
+  deleteItemsReview(value: string): void {
+    const index = this.review.indexOf(value);
+
+    if (index >= 0) {
+      this.review.splice(index, 1);
+    }
+  }
+
+  deleteItemsDone(value: string): void {
+    const index = this.done.indexOf(value);
+
+    if (index >= 0) {
+      this.done.splice(index, 1);
+    }
   }
 }
--- src/app/table/table.module.ts
+++ src/app/table/table.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainTableComponent } from './components/main.table/main.table.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -11,6 +17,12 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
   imports: [
     CommonModule,
     DragDropModule,
+    FormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
   ],
   exports: [
     MainTableComponent,
